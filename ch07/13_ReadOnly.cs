0c09e9cf3fc915250962dd53c73ca88cf1ca9b68
이정철 <ghop0822@gmail.com>
2026-06-23 12:31:26 +0900

    Add files via upload

---
+++ ch07/13_ReadOnly.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Numerics;
+
+class ReadOnly
+{
+    private readonly int min;
+    private readonly int max;
+
+    public ReadOnly(int v1, int v2) //매개변수로 값을 초기화 하자 
+    {
+        min = v1; //이렇게 생성자 안에서만 값을 초기화 할 수 있다.
+        max = v2;
+    }
+
+   /* public void ChangeMax(int newMax)
+    {
+        max = newMax;  요건 오류 생성자가 아닌 다른 곳은 에러가 발생
+    }*/
+}
+
+class MainApp
+{
+    static void Main()
+    {
+        ReadOnly c = new ReadOnly(100, 10);
+    }
+}
