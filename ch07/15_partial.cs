eea003ee6db853b07f70c2e207d05fe88d1b878f
이정철 <ghop0822@gmail.com>
2026-06-23 15:53:13 +0900

    Add files via upload

---
+++ ch07/15_partial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+partial class MyClass
+{
+    public void Method1()
+    {
+        Console.WriteLine("Method1");
+    }
+}
+
+partial class MyClass
+{
+    public void Method2()
+    {
+        Console.WriteLine("Method2");
+    }
+}
+
+partial class MyClass
+{
+    public void Method3()
+    {
+        Console.WriteLine("Method3");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        MyClass my = new MyClass();
+
+        my.Method1();
+        my.Method2();
+        my.Method3();
+    }
+}
