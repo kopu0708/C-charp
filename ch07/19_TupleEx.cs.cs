c77c04b23c3092897069d1c67dfa0c6278c8dfb0
이정철 <ghop0822@gmail.com>
2026-06-25 12:07:41 +0900

    Add files via upload

---
+++ ch07/19_TupleEx.cs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var a = ("아이", 999); //명명되지 않은 튜플
+        var b = (Feel: "신나", num: 220); //명명된 튜플
+        var (Feel, num) = ("좋아", 3030);//분해 
+        var c = ("싫어", 30);
+
+        //명명된 튜플과 되지않은 튜플 할당
+        c = b;
+        //이렇게도 되네 
+        c = (Feel,num);
+
+        Console.WriteLine($"{a.Item1}, {b.Feel}"); //명명된 튜플과 명명되지 않은 튜플의 호출방식이 다르다.
+        Console.WriteLine($"{a.Item1}, {c.Item1}"); //자료형과 수가 같아서 할당된거 확인
+
+
+    }
+}
