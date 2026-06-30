b2882fb87890cb68546f38d85eb8ab85ee487665
이정철 <ghop0822@gmail.com>
2026-06-30 14:01:28 +0900

    Add files via upload

---
+++ ch09/08_AnonymousType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var a = new { Name = "박상현", Age = 123 }; // 이게 무명 형식 
+        Console.WriteLine($"Name:{a.Name}, Age:{a.Age}");
+
+        var b = new { Subject = "수학", Scores = new int[] { 90, 80, 70, 60 } };
+
+        Console.WriteLine($"Subject:{b.Subject}, Scores: ");
+        foreach (var score in b.Scores)
+            Console.Write($"{score} ");
+
+        Console.WriteLine();
+    }
+}
