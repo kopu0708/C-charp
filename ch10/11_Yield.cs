33934341796b00d48c3f66f1dd3e13d5338430fb
이정철 <ghop0822@gmail.com>
2026-07-06 22:14:00 +0900

    Add files via upload

---
+++ ch10/11_Yield.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+class MyEnumerator : IEnumerable
+{
+    int[] numbers = { 1, 2, 3, 4 };
+    public IEnumerator GetEnumerator()
+    {
+        yield return numbers[0];
+        yield return numbers[1];
+        yield return numbers[2];
+        yield break; // GetEnumerator() 메소드를 종료시킨다.
+        yield return numbers[3]; //따라서 이건 실행안됨
+    }
+}
+
+class MainApp
+{
+    static void Main()
+    {
+        var obj = new MyEnumerator(); 
+        foreach(int i in obj)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
