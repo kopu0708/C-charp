b08fdd1b50c3625aced8821aae5425209f4b1b9d
이정철 <ghop0822@gmail.com>
2026-07-13 21:37:02 +0900

    Add files via upload

---
+++ ch12/01_TryCatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 1, 2, 3 };
+
+        try
+        {
+            for(int i = 0; i<5; i++)
+            {
+                Console.WriteLine(arr[i]); //i가 3이 되면 IndexOutOfRangeException 이라는 객체가 던져지고 
+            }
+        }
+
+        catch (IndexOutOfRangeException e) //그걸 여기서 받는다. 
+        {
+            Console.WriteLine($"예외가 발생했습니다 : {e.Message}");
+        }
+
+        Console.WriteLine("종료");
+    }
+}
