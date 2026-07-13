a85a1dedd3c36cbfb30ae3a1cfef931f3e11b9bf
이정철 <ghop0822@gmail.com>
2026-07-13 16:15:04 +0900

    Add files via upload

---
+++ ch11/04_ListEx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic; //이거 쓸려면 이거 있어야함
+
+class Program
+{
+    static void Main()
+    {
+        List<int> list = new List<int>();
+        for (int i = 0; i < 5; i++)
+        {
+            list.Add(i);
+        }
+
+        foreach (int element in list)
+            Console.Write($"{element} ");
+        Console.WriteLine();
+
+        list.RemoveAt(2);
+
+        foreach (int element in list)
+            Console.Write($"{element} ");
+        Console.WriteLine();
+
+        list.Insert(2, 2);
+
+        foreach (int element in list)
+            Console.Write($"{element} ");
+        Console.WriteLine();
+    }
+}
