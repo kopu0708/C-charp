996f42734bc33448999eb43ceca758096ea4ff11
이정철 <ghop0822@gmail.com>
2026-07-13 19:18:53 +0900

    Add files via upload

---
+++ ch11/07_DictionaryEx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, string> dic = new Dictionary<string, string>();
+        dic["하나"] = "one";
+        dic["둘"] = "two";
+        dic["셋"] = "three";
+        dic["넷"] = "four";
+        dic["다섯"] = "five";
+
+        Console.WriteLine(dic["하나"]);
+        Console.WriteLine(dic["둘"]);
+        Console.WriteLine(dic["셋"]);
+        Console.WriteLine(dic["넷"]);
+        Console.WriteLine(dic["다섯"]);
+
+    }
+}
