d9e07de1f7485eb45f73cd0192077f4f32d3ab94
이정철 <ghop0822@gmail.com>
2026-07-13 19:07:32 +0900

    Add files via upload

---
+++ ch11/05_QueueEx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Queue<int> queue = new Queue<int>();
+
+        queue.Enqueue(1); //큐에 쌓기
+        queue.Enqueue(2);
+        queue.Enqueue(3);
+        queue.Enqueue(4);
+        queue.Enqueue(5); //이 친구가 가장 마지막에 나옴
+
+        while (queue.Count > 0)
+            Console.WriteLine(queue.Dequeue()); //큐에 있는거 순서대로 꺼내기
+    }
+}
