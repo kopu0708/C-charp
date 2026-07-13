e8a93f8f2b34469d6b20927ac935346c6602fbad
이정철 <ghop0822@gmail.com>
2026-07-13 19:10:12 +0900

    Add files via upload

---
+++ ch11/06_StackEx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Stack<int> stack = new Stack<int>();
+
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+        stack.Push(4);
+        stack.Push(5);
+
+        while (stack.Count > 0)
+            Console.WriteLine(stack.Pop());
+
+    }
+}
