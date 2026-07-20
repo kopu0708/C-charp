2fa95ab2d0f960f1d29143947a9cc59fa02ba6eb
이정철 <ghop0822@gmail.com>
2026-07-20 19:34:48 +0900

    Add files via upload

---
+++ ch14/01_Lambda.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    delegate int Calculate(int a, int b);
+
+    static void Main()
+    {
+        Calculate calc = (a, b) => a + b; // 매우 간결한 매소드와 매우 간결하게 선언된 모습 
+
+        Console.WriteLine($"{3} + {4} : {calc(3, 4)}");
+    }
+}
