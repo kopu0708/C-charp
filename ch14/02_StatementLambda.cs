474139d5a0b2a5a7c0733b7a4ef15ec70c11e70b
이정철 <ghop0822@gmail.com>
2026-07-22 08:54:19 +0900

    Add files via upload

---
+++ ch14/02_StatementLambda.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    delegate string Concatenate(string[] args);
+
+    static void Main(string[] args)
+    {
+        string array =  Console.ReadLine();
+        string[] words = array.Split(' ');
+            
+        Concatenate concat = 
+            (arr) =>
+            {
+                string result = "";
+                foreach (string s in arr)
+                    result += s;
+
+                return result;
+            };
+
+        Console.WriteLine(concat(words));
+    }
+}
