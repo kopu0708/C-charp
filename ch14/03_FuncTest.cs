1a446836001c1fa512e33649ff4a097c4d1c3141
이정철 <ghop0822@gmail.com>
2026-07-22 21:50:29 +0900

    Add files via upload

---
+++ ch14/03_FuncTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Func<int> func1 = () => 10; // 바로 10을 반환 
+        Console.WriteLine($"func1() : {func1()}"); 
+
+        Func<int, int> func2 = (x) => x * 2; // 정수형을 받아 정수형으로 반환 
+        Console.WriteLine($"func2(4) : {func2(4)}");
+
+        Func<int, int, string> func3 = (x, y) => (x + y).ToString(); // 정수형을 더해서 문자열로 반환하기에 형 변환을 해줘야 한다. 
+        Console.WriteLine($"func3(22,7) : {func3(22, 7)}");
+    }
+}
